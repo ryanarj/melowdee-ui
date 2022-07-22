3bc895f906a18e339f8d6c9079200b06d64772c1
Ryan Arj <ryanarj@Ryans-MBP-2.home>
2022-07-22 12:39:02 -0400

    sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,123 @@
-import './App.css';
+import {
+  makeStyles,
+  Container,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+
+interface IFormInput {
+  email: string;
+  userName: string;
+  password: string;
+  age: string;
+}
+
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  userName: yup.string().required().min(2).max(25),
+  password: yup.string().required().min(8).max(120),
+  age: yup.string().required().min(2).max(25)
+});
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    textAlign: "center",
+    margin: theme.spacing(1, 0, 4),
+  },
+  submitButton: {
+    marginTop: theme.spacing(4),
+  },
+}));
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
+
+  const { heading, submitButton } = useStyles();
+
+  const [json, setJson] = useState<string>();
+
+  const onSubmit = (data: IFormInput) => {
+    setJson(JSON.stringify(data));
+  };
+
   return (
-    <div className="App">
-    </div>
+    <Container maxWidth="xs">
+      <Typography className={heading} variant="h3">
+        Sign Up Form
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <TextField
+          {...register("email")}
+          variant="outlined"
+          margin="normal"
+          label="Email"
+          helperText={errors.email?.message}
+          error={!!errors.email?.message}
+          fullWidth
+          required
+        />
+        <TextField
+          {...register("userName")}
+          variant="outlined"
+          margin="normal"
+          label="Username"
+          helperText={errors.userName?.message}
+          error={!!errors.userName?.message}
+          fullWidth
+          required
+        />
+        <TextField
+          {...register("age")}
+          variant="outlined"
+          margin="normal"
+          label="Age"
+          helperText={errors.age?.message}
+          error={!!errors.age?.message}
+          fullWidth
+          required
+        />
+        <TextField
+          {...register("password")}
+          variant="outlined"
+          margin="normal"
+          label="Password"
+          helperText={errors.password?.message}
+          error={!!errors.password?.message}
+          type="password"
+          fullWidth
+          required
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={submitButton}
+        >
+          Sign Up
+        </Button>
+        {json && (
+          <>
+            <Typography variant="body1">
+              Below is the JSON that would normally get passed to the server
+              when a form gets submitted
+            </Typography>
+            <Typography variant="body2">{json}</Typography>
+          </>
+        )}
+      </form>
+    </Container>
   );
 }
 

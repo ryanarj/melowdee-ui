d72268f871ed982560ce1d317d5ceec1f79fe361
Ryan Arj <ryanarj@Ryans-MBP-2.home>
2022-07-25 13:48:08 -0400

    new files

---
+++ src/pages/profilePage.tsx
@@ -0,0 +1,69 @@
+import {
+    makeStyles,
+    Container,
+    Typography,
+    TextField,
+    Button,
+  } from "@material-ui/core";
+  import { useForm } from "react-hook-form";
+  import * as yup from "yup";
+  import { yupResolver } from "@hookform/resolvers/yup";
+  import { useState } from "react";
+  
+  interface IFormInput {
+    email: string;
+    username: string;
+    password: string;
+    age: string;
+  }
+  
+  const schema = yup.object().shape({
+    email: yup.string().required().email(),
+    username: yup.string().required().min(2).max(25),
+    password: yup.string().required().min(8).max(120),
+    age: yup.string().required().min(2).max(25)
+  });
+  
+  const useStyles = makeStyles((theme) => ({
+    heading: {
+      textAlign: "center",
+      margin: theme.spacing(1, 0, 4),
+    },
+    submitButton: {
+      marginTop: theme.spacing(4),
+    },
+  }));
+  
+  function ProfilePage() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<IFormInput>({
+      resolver: yupResolver(schema),
+    });
+  
+    const { heading, submitButton } = useStyles();
+  
+    const [json, setJson] = useState<string>();
+  
+    async function signUp(){
+  
+   }
+  
+    const onSubmit = (data: IFormInput) => {
+      console.log(data)
+      setJson(JSON.stringify(data));
+      signUp();
+    };
+  
+    return (
+      <Container maxWidth="xs">
+        <Typography className={heading} variant="h3">
+          Profile Page
+        </Typography>
+      </Container>
+    );
+  }
+  
+  export default ProfilePage;
---
+++ src/pages/signUpPage.tsx
@@ -0,0 +1,147 @@
+import {
+    makeStyles,
+    Container,
+    Typography,
+    TextField,
+    Button,
+  } from "@material-ui/core";
+  import { useForm } from "react-hook-form";
+  import * as yup from "yup";
+  import { yupResolver } from "@hookform/resolvers/yup";
+  import { useState } from "react";
+  
+  interface IFormInput {
+    email: string;
+    username: string;
+    password: string;
+    age: string;
+  }
+  
+  const schema = yup.object().shape({
+    email: yup.string().required().email(),
+    username: yup.string().required().min(2).max(25),
+    password: yup.string().required().min(8).max(120),
+    age: yup.string().required().min(2).max(25)
+  });
+  
+  const useStyles = makeStyles((theme) => ({
+    heading: {
+      textAlign: "center",
+      margin: theme.spacing(1, 0, 4),
+    },
+    submitButton: {
+      marginTop: theme.spacing(4),
+    },
+  }));
+  
+  function SignUpPage() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<IFormInput>({
+      resolver: yupResolver(schema),
+    });
+  
+    const { heading, submitButton } = useStyles();
+  
+    const [json, setJson] = useState<string>();
+  
+    async function signUp(){
+  
+      const response = await fetch('http://127.0.0.1:8000/user_signup/', {
+        method: 'POST',
+        body: json,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'} 
+      });
+  
+      console.log(json)
+  
+      if (!response.ok) { /* Handle */ }
+  
+      // If you care about a response:
+      if (response.body !== null) {
+        // body is ReadableStream<Uint8Array>
+        // parse as needed, e.g. reading directly, or
+        const asString = new (String as any).TextDecoder("utf-8").decode(response.body) ;
+      }
+  
+   }
+  
+    const onSubmit = (data: IFormInput) => {
+      console.log(data)
+      setJson(JSON.stringify(data));
+      signUp();
+    };
+  
+    return (
+      <Container maxWidth="xs">
+        <Typography className={heading} variant="h3">
+          Sign Up Form
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <TextField
+            {...register("email")}
+            variant="outlined"
+            margin="normal"
+            label="Email"
+            helperText={errors.email?.message}
+            error={!!errors.email?.message}
+            fullWidth
+            required
+          />
+          <TextField
+            {...register("username")}
+            variant="outlined"
+            margin="normal"
+            label="Username"
+            helperText={errors.username?.message}
+            error={!!errors.username?.message}
+            fullWidth
+            required
+          />
+          <TextField
+            {...register("age")}
+            variant="outlined"
+            margin="normal"
+            label="Age"
+            helperText={errors.age?.message}
+            error={!!errors.age?.message}
+            fullWidth
+            required
+          />
+          <TextField
+            {...register("password")}
+            variant="outlined"
+            margin="normal"
+            label="Password"
+            helperText={errors.password?.message}
+            error={!!errors.password?.message}
+            type="password"
+            fullWidth
+            required
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={submitButton}
+          >
+            Sign Up
+          </Button>
+          {json && (
+            <>
+              <Typography variant="body1">
+                Below is the JSON that would normally get passed to the server
+                when a form gets submitted
+              </Typography>
+              <Typography variant="body2">{json}</Typography>
+            </>
+          )}
+        </form>
+      </Container>
+    );
+  }
+  
+  export default SignUpPage;

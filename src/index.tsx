035bb0599e04bb78d6d4a74e29e2138073bc61c8
Ryan Arj <ryanarj@Ryans-MBP-2.home>
2022-07-25 13:47:36 -0400

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import App from "./App";
+import SignUpPage from "./pages/signUpPage";
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <SignUpPage />
   </React.StrictMode>,
   document.getElementById("root")
 );

eb9fd13e85016237253d51cca0a86fc712032676
ryanarj <ryanarj07@gmail.com>
2022-08-03 15:43:02 -0400

    appbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,51 @@ import SignUpPage from "./pages/signUpPage";
 import LoginPage from "./pages/loginPage";
 import ProfilePage from "./pages/profilePage";
 
+import { AppBar, Toolbar, IconButton, Typography, MenuItem } from "@material-ui/core";
+
+import { ThemeProvider, createTheme } from "@mui/material";
+import { orange, green } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette :{
+    primary : {
+      main: orange[400]
+    },
+    secondary: {
+      main: green[400]
+    }
+  }
+})
+
 
 
  function App(){
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signUp" element={<SignUpPage />} />
-        <Route path="/profilePage" element={<ProfilePage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-Header">
+          <AppBar  color={"primary"}>
+            <Toolbar>
+              <IconButton>
+              </IconButton>
+              <Typography variant="h6">
+                Melowdee
+              </Typography>
+              <MenuItem>
+                Login
+              </MenuItem>
+            </Toolbar>
+          </AppBar>
+        </header>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/signUp" element={<SignUpPage />} />
+            <Route path="/profilePage" element={<ProfilePage />} />
+          </Routes>
+        </Router>
+      </div>
+    </ThemeProvider>
   )
 }
 

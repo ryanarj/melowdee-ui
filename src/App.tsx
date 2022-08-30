79a929895decb15f7a52610f92b3e99a2e4d64f2
ryanarj <ryanarj07@gmail.com>
2022-08-30 16:22:30 -0400

    search bar

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ import AlbumPage from "./pages/albumPage";
 import SongPage from "./pages/songPage";
 import { useNavigate } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, MenuItem } from "@material-ui/core";
-
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { red } from "@mui/material/colors";
 
@@ -23,6 +25,49 @@ const theme = createTheme({
     },
   },
 })
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 function HomeIcon(props: SvgIconProps) {
   return (
@@ -55,6 +100,15 @@ function HomeIcon(props: SvgIconProps) {
               <Typography variant="h6">
                 Melowdee
               </Typography>
+              <Search>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
             </Toolbar>
           </AppBar>
         </header>
